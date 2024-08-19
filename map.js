06d308bd8ab332b9291c15c6000055014b7829c7
Sean Patton <seanpatton1@outlook.com>
2024-08-19 20:39:11 +0000

    mpa method

--- map.js
+++ map.js
@@ -5,16 +5,30 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
 
+console.log(results);
 
 // Using map()
+const multByTwo = function (num){
+  return num * 2;
+}
 
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
 
+const resultsOne = nums.map(function (num){ return num * 2; });
+console.log(resultsOne);
 
 // Simplfied w/ map() + arrow function
-
+const resultsTwo = nums.map(num => num * 2);
+console.log(resultsTwo);
 
 // With objects:
 const students = [
@@ -37,3 +51,19 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+
+const studentsWithIds = students.slice(0, 2).map(student => ({
+  id: student.id,
+  name: student.name
+}));
+console.log(studentsWithIds);
+
+const ages = [24, 25, 26];
+
+const studentsWithIdsTwo = students.map((student, index) => ({
+  ...student,
+  age: ages[index]
+})); 
+
+console.log(studentsWithIdsTwo);
